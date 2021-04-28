0670aaad7b23d7e77a14980ab304070f9b011ce2
aldipranaja <43459276+aldipranaja@users.noreply.github.com>
2021-04-28 16:53:01 +0700

    Add files via upload

---
+++ UTS_Bigdata.py
@@ -0,0 +1,12 @@
+x=input("Masukkan Kata :") #input
+
+uts=x.split() #merubah jadi list
+
+y=len(uts) #menghitung total kata
+
+kalimat= uts 
+
+for isi in kalimat:
+    print (isi) #dijadikan perulangan untuk hasil print menurun
+    
+print ("Panjang Kata :", y)
